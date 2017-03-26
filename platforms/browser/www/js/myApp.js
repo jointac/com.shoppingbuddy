64f6e7d869c13b8ff3aa5f3bf50e939966e0b1aa
Johanna Intac <johanna@minerlabs.com>
2017-03-27 00:58:55 +0800

    touch up design, function

--- platforms/browser/www/js/myApp.js
+++ platforms/browser/www/js/myApp.js
@@ -29,6 +29,254 @@ myApp
         }];
     return promoitems;
 })
+.factory('aisle', function(){
+    var aisleitem = [{
+            "aisle":"1",
+            "cat":"FRESH PRODUCE",
+            "description":"FRESH FRUITS, FRESH HERBS, FRESH VEGETABLES",
+            "itemimg":"/img/produce2.png",
+        },{
+            "aisle":"2",
+            "cat":"MEAT & SEAFOOD",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/meat2.png",
+        },{
+            "aisle":"3",
+            "cat":"DAIRY & EGGS",
+            "description":"CONDENSED MILK, CREAM, EGGS, EVAPORATED MILK",
+            "itemimg":"/img/dairy2.png",
+        },{
+            "aisle":"4",
+            "cat":"BAKERY",
+            "description":"BREAD, BUNS & ROLLS, TORTILLA & FLAT BREAD",
+            "itemimg":"/img/bakery2.png",
+        },{
+            "aisle":"5",
+            "cat":"BREAKFAST",
+            "description":"CEREAL, HOT CEREAL & PANCAKES",
+            "itemimg":"/img/meat2.png",
+        },{
+            "aisle":"6",
+            "cat":"FROZEN",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/bfast2.png",
+        },{
+            "aisle":"7",
+            "cat":"PANTRY",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/pantry2.png",
+        },{
+            "aisle":"8",
+            "cat":"CANNED GOODS",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/can2.png",
+        },{
+            "aisle":"9",
+            "cat":"PASTA & DRIED",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/pasta2.png",
+        },{
+            "aisle":"10",
+            "cat":"BEVERAGES",
+            "description":"HOTDOGS, BACON & SAUSAGES, MEAT COUNTER, POULTRY",
+            "itemimg":"/img/beverages2.png",
+        }];
+    return aisleitem;
+})
+.factory('grocery', function(){
+    var groceryitem = {};
+    groceryitem.freshproduce = function() {
+        var items = [{
+            "item":1,
+            "description":"Apple",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Cabbage",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    groceryitem.meatseafood = function() {
+        var items = [{
+            "item":1,
+            "description":"Tilapia",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Minced Meat",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    groceryitem.dairyegg = function() {
+        var items = [{
+            "item":1,
+            "description":"Egg",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Milk",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    groceryitem.bakery = function() {
+        var items = [{
+            "item":1,
+            "description":"Bread",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Buns",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    groceryitem.breakfast = function() {
+        var items = [{
+            "item":1,
+            "description":"Cereal",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Pancake",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    groceryitem.frozen = function() {
+        var items = [{
+            "item":1,
+            "description":"Hotdogs",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Poultry",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+    groceryitem.pantry = function() {
+        var items = [{
+            "item":1,
+            "description":"Flour",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Flour",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+    groceryitem.canneditem = function() {
+        var items = [{
+            "item":1,
+            "description":"Century Tuna",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Corned Beef",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+    groceryitem.driedfood = function() {
+        var items = [{
+            "item":1,
+            "description":"Pasta",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Rice",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+    groceryitem.beverage = function() {
+        var items = [{
+            "item":1,
+            "description":"Coke",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }, {
+            "item":2,
+            "description":"Iced Tea",
+            "price":5,
+            "weight":1,
+            "nutritionfacts":"item"
+        }];
+        return items;
+    };
+
+    return groceryitem;
+})
+.factory('newitem', function(){
+    var newitems = [{
+            "itemdesc":"MAGGI MAGIC ISDA",
+            "itemamt":"P36.00",
+            "promodetail":"8G 12'S",
+            "itemimg":"/img/coke.png"
+        },{
+            "itemdesc":"OREO",
+            "itemamt":"P10.00",
+            "promodetail":"50G",
+            "itemimg":"/img/oreo.png"
+        },{
+            "itemdesc":"DOVE",
+            "itemamt":"P45.00",
+            "promodetail":"",
+            "itemimg":"/img/dove.png"
+        },{
+            "itemdesc":"JOY DISHWASHING LIQUID",
+            "itemamt":"P40.00",
+            "promodetail":"3G",
+            "itemimg":"/img/joy.png"
+        }];
+    return newitems;
+})
 .factory('globalbudget', function(newbudget){
 	var newitem = newbudget;
 	return newitem;
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 myApp
-.controller("splashpageCtrl", function($scope, $timeout, promoitems) {
+.controller("splashpageCtrl", function($scope, $timeout, promoitems, newitem, aisle, grocery) {
 	$scope.welcomepage = true;
 	$scope.detailpage = false;
 	$timeout(function(){
@@ -8,44 +8,111 @@ myApp
     },2500)
 
     var ctrl = this;
+    var budget = 0;
 	$scope.error = false;
-	$scope.pricingdetail = true;
-	$scope.shoppinglinks = false;
-	$scope.promoitems = false;
+	$scope.pricingdetail = false;
+	$scope.detailpage = false;
+	$scope.shoppinglinks = true;
+	$scope.aislepage = false;
+	$scope.promopage = false;
+	$scope.whatsnewpage =false;
+	$scope.productcat1 = false;
+	$scope.aisleitems = aisle;
+	$scope.shoppingcart = [];
+	//productcat1
+	$scope.windowheight = ($(window).height() - 50).toString() + "px";
 	$scope.proceednow = function() {
 		if ($.isNumeric(ctrl.txtAmt)) {
+			budget = ctrl.txtAmt;
 			$scope.error = false;
 			$scope.pricingdetail = false;
-			$scope.shoppinglinks = true;
-			$scope.windowheight = ($(window).height() - 50).toString() + "px";
+			$scope.productcat1 = true;
 		} else {
 			$scope.error = true;
 		}
-	}
+	};
 	$scope.goto = function(link) {
 		var $url = 'shoppinglinks';
+		$scope.pricingdetail = false;
 		$scope.shoppinglinks = false;
+		$scope.promoitems = false;
+		$scope.aislepage = false;
+		$scope.promopage = false;
+		$scope.productcat1 = false;
+		$scope.whatsnewpage = false;
 		switch(link) {
 			case 0:
 				$scope.pricingdetail = false;
 				$scope.shoppinglinks = true;
 				$scope.promoitems = false;
+				$scope.aislepage = false;
+				$scope.promopage = false;
+				break;
+			case 1:
+				$scope.whatsnewpage = true;
+				$scope.whatsnew = newitem;
+				break;
 			case 2:
-				$url = 'aisle';
+				$scope.aislepage = true;
+				$scope.detailheight = (($(window).height() - 50) - 70).toString() + "px";
+				break;
+			case 3:
+				$scope.pricingdetail = true;
 				break;
 			case 4:
-				$scope.promoitems = true;
-				$url = 'promoitems';
 				$scope.detailheight = (($(window).height() - 50) - 100 - 100).toString() + "px";
-				$scope.aisleitems = promoitems;
+				$scope.promopage = true;
+				$scope.prmitems = promoitems;
 				break;
 		}
 	}
-		
-	//alert($globalbudget);
+	$scope.gotocat = function(link) {
+		switch (link) {
+			case "1":
+				$scope.groceryitem = grocery.freshproduce();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "2":
+				$scope.groceryitem = grocery.meatseafood();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "3":
+				$scope.groceryitem = grocery.dairyegg();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "4":
+				$scope.groceryitem = grocery.bakery();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "5":
+				$scope.groceryitem = grocery.breakfast();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "6":
+				$scope.groceryitem = grocery.frozen();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "7":
+				$scope.groceryitem = grocery.pantry();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "8":
+				$scope.groceryitem = grocery.canneditem();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "9":
+				$scope.groceryitem = grocery.driedfood();
+				alert($scope.groceryitem[0].description);
+				break;
+			case "10":
+				$scope.groceryitem = grocery.beverage();
+				alert($scope.groceryitem[0].description);
+				break;
+		}
+	}
 })
 .controller('headerCtrl', function($scope){
-	var dayarr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+	var dayarr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 	var newDate = new Date();
 	var daytoday = dayarr[newDate.getDay()];
 	$scope.timenow = daytoday + ", " + newDate.getHours() + ":" + newDate.getMinutes();
